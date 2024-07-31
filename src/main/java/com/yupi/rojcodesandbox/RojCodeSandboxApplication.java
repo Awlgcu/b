190db269019f3a586accc5d478ec2d964710c2ca
Rick Ruo <2454618331@qq.com>
2024-08-01 02:44:24 +0800

    无

---
+++ src/main/java/com/yupi/rojcodesandbox/RojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.rojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yupi/rojcodesandbox/RojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.rojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
